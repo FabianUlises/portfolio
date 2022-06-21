c20684f39fb089e277890e645ed11c0463ff0fd9
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-21 12:48:41 -0700

    Finished nav button.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+// Defining variables
 const btn = document.querySelector('.btn');
 const navMenu = document.querySelector('.nav-menu');
 
+// Btn function to toggle nav
 btn.addEventListener('click', () => {
-    console.log('button clicked!')
     navMenu.classList.toggle('toggle-nav');
+    btn.classList.toggle('toggle');
+
 });
