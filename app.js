32b6f7bea28dfdb8e52464f7d3de21ee2c763841
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-06-27 21:59:11 -0700

    Modified mobile navbar

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-// Defining variables
-const btn = document.querySelector('.btn');
-const navMenu = document.querySelector('.nav-menu');
-
-// Btn function to toggle nav
-btn.addEventListener('click', () => {
-    navMenu.classList.toggle('toggle-nav');
-    btn.classList.toggle('toggle');
-
-});
+const primaryNav = document.querySelector('.primary-nav');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+navToggle.addEventListener('click', () => {
+    navToggle.classList.toggle('toggle');
+    const visibility = primaryNav.getAttribute('data-visible');
+    
+    if(visibility === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true)
+    } else if (visibility === 'true') {
+        primaryNav.setAttribute('data-visible', false)
+        navToggle.setAttribute('aria-expanded', false)
+    }
+})
